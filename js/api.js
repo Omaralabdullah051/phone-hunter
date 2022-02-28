efd05c5a6cfb0ef403e6af518e62345fdb84c57e
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-02-28 10:33:53 -0800

    update

--- js/api.js
+++ js/api.js
@@ -1,16 +1,40 @@
+//handle error
+const error = text => {
+    document.getElementById('error').innerText = text;
+};
+//handle spinner
+const spinner = displayType => {
+    document.getElementById('spinner').style.display = displayType;
+}
 
 //Load phones
 const loadPhones = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
-        .then(res => res.json())
-        .then(data => displayPhones(data.data))
+    spinner('block');
+    if (isNaN(searchField.value !== true || searchField.value === '')) {
+        error('Please input phone name');
+        spinner('none');
+        document.getElementById('phones-container').textContent = '';
+        document.getElementById('phone-details-container').textContent = '';
+    }
+    else {
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+            .then(res => res.json())
+            .then(data => displayPhones(data.data))
+
+    }
+    searchField.value = '';
 };
 //Display phones on UI
 const displayPhones = phones => {
+    console.log(phones);
     const container = document.getElementById('phones-container');
-    phones.forEach(phone => {
+    container.textContent = '';
+    error('Please input phone name');
+    spinner('none');
+    document.getElementById('phone-details-container').textContent = '';
+    phones?.forEach(phone => {
         console.log(phone);
         const div = document.createElement('div');
         div.innerHTML = `
@@ -26,10 +50,13 @@ const displayPhones = phones => {
     </div>
     `;
         container.appendChild(div);
+        spinner('none');
+        error('');
     })
 };
 //Load phone details 
 const loadPhoneDetails = phone => {
+    spinner('block');
     fetch(`https://openapi.programming-hero.com/api/phone/${phone}`)
         .then(res => res.json())
         .then(data => displayPhoneDetails(data.data))
@@ -39,6 +66,10 @@ const loadPhoneDetails = phone => {
 const displayPhoneDetails = phone => {
     console.log(phone);
     const container = document.getElementById('phone-details-container');
+    container.textContent = '';
+    spinner('none');
+    error('');
+    document.getElementById('phones-container').textContent = '';
     const div = document.createElement('div');
     div.innerHTML = `
     <div class="card w-50 mx-auto ">
@@ -47,16 +78,16 @@ const displayPhoneDetails = phone => {
             <h5 class="card-title"><span class="fw-bolder">Name:</span> ${phone.name}</h5>
             <h6 class="card-text"><span class="fw-bolder">Brand:</span> ${phone.brand}</h6>
             <p class="card-text"><span class="fw-bolder">Release Date:</span> ${phone.releaseDate ? phone.releaseDate : 'No release date found'}</p>
-            <p class="card-text"><span class="fw-bolder">Chipset:</span> ${phone.mainFeatures.chipSet}</p>
-            <p class="card-text"><span class="fw-bolder">Display Size:</span> ${phone.mainFeatures.displaySize}</p>
-            <p class="card-text"><span class="fw-bolder">Memory:</span> ${phone.mainFeatures.memory}</p>
-            <p class="card-text"><span class="fw-bolder">Sensors:</span> ${phone.mainFeatures.sensors}</p>
-            <p class="card-text"><span class="fw-bolder">Bluetooth:</span> ${phone.others.Bluetooth}</p>
-            <p class="card-text"><span class="fw-bolder">GPS:</span> ${phone.others.GPS}</p>
-            <p class="card-text"><span class="fw-bolder">NFC:</span> ${phone.others.NFC}</p>
-            <p class="card-text"><span class="fw-bolder">Radio:</span> ${phone.others.Radio}</p>
-            <p class="card-text"><span class="fw-bolder">USB:</span> ${phone.others.USB}</p>
-            <p class="card-text"><span class="fw-bolder">WLAN:</span> ${phone.others.WLAN}</p>
+            <p class="card-text"><span class="fw-bolder">Chipset:</span> ${phone.mainFeatures.chipSet ? phone.mainFeatures.chipSet : ''}</p>
+            <p class="card-text"><span class="fw-bolder">Display Size:</span> ${phone.mainFeatures.displaySize ? phone.mainFeatures.displaySize : ''}</p>
+            <p class="card-text"><span class="fw-bolder">Memory:</span> ${phone.mainFeatures.memory ? phone.mainFeatures.memory : ''}</p>
+            <p class="card-text"><span class="fw-bolder">Sensors:</span> ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors : ''}</p>
+            <p class="card-text"><span class="fw-bolder">Bluetooth:</span> ${phone.others.Bluetooth ? phone.others.Bluetooth : ''}</p>
+            <p class="card-text"><span class="fw-bolder">GPS:</span> ${phone.others.GPS ? phone.others.GPS : ''}</p>
+            <p class="card-text"><span class="fw-bolder">NFC:</span> ${phone.others.NFC ? phone.others.NFC : ''}</p>
+            <p class="card-text"><span class="fw-bolder">Radio:</span> ${phone.others.Radio ? phone.others.Radio : ''}</p>
+            <p class="card-text"><span class="fw-bolder">USB:</span> ${phone.others.USB ? phone.others.USB : ''}</p>
+            <p class="card-text"><span class="fw-bolder">WLAN:</span> ${phone.others.WLAN ? phone.others.WLAN : ''}</p>
             
         </div>   
     </div>
