e63e87b75458435b9badba90465974af6872508f
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-02-28 21:27:48 -0800

    updated-display-phone

--- js/api.js
+++ js/api.js
@@ -6,17 +6,23 @@ const error = text => {
 const spinner = displayType => {
     document.getElementById('spinner').style.display = displayType;
 }
+//handle text content
+const textContent = () => {
+    document.getElementById('phones-container').textContent = '';
+    document.getElementById('phone-details-container').textContent = '';
+    document.getElementById('more-phone-container').textContent = '';
+}
 
 //Load phones
 const loadPhones = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     spinner('block');
-    if (isNaN(searchField.value !== true || searchField.value === '')) {
-        error('Please input phone name');
+    if (searchField.value === '') {
         spinner('none');
-        document.getElementById('phones-container').textContent = '';
-        document.getElementById('phone-details-container').textContent = '';
+        error('No result found');
+        textContent();
+        document.getElementById('see-more-btn').style.display = 'none';
     }
     else {
         fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
@@ -26,34 +32,69 @@ const loadPhones = () => {
     }
     searchField.value = '';
 };
+
 //Display phones on UI
 const displayPhones = phones => {
     console.log(phones);
     const container = document.getElementById('phones-container');
-    container.textContent = '';
-    error('Please input phone name');
     spinner('none');
-    document.getElementById('phone-details-container').textContent = '';
-    phones?.forEach(phone => {
+    error('No result found');
+    textContent();
+    const maximumPhones = phones.slice(0, 20);
+    maximumPhones?.forEach(phone => {
         console.log(phone);
         const div = document.createElement('div');
         div.innerHTML = `
-    <div class="col mb-3">
-        <div class="card h-100 ">
-            <img src="${phone.image}" class="card-img-top" alt="...">
-            <div class="card-body ">
-                <h5 class="card-title">${phone.phone_name}</h5>
-                <p class="card-text">${phone.brand}</p>
-                <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-lg btn-success">Details</button>
+        <div class="col mb-3">
+            <div class="card h-100 ">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body ">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">${phone.brand}</p>
+                    <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-lg btn-success">Details</button>
+                </div>
             </div>
         </div>
-    </div>
-    `;
+        `;
         container.appendChild(div);
         spinner('none');
-        error('');
+        error(`${phones.length} results found`);
     })
+    if (phones.length > 20) {
+        document.getElementById('see-more-btn').style.display = 'block';
+        document.getElementById('see-more-btn').addEventListener('click', function () {
+            // const container = document.getElementById('phones-container');
+            const container = document.getElementById('more-phone-container');
+            container.textContent = '';
+            const otherPhones = phones.slice(20);
+            otherPhones?.forEach(phone => {
+                console.log(phone);
+                const div = document.createElement('div');
+                div.innerHTML = `
+                <div class="col mb-3">
+                    <div class="card h-100 ">
+                        <img src="${phone.image}" class="card-img-top" alt="...">
+                        <div class="card-body ">
+                            <h5 class="card-title">${phone.phone_name}</h5>
+                            <p class="card-text">${phone.brand}</p>
+                            <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-lg btn-success">Details</button>
+                        </div>
+                    </div>
+                </div>
+                `;
+                container.appendChild(div);
+                document.getElementById('see-more-btn').style.display = 'none';
+
+
+            })
+        })
+    }
 };
+
+
+
+
+
 //Load phone details 
 const loadPhoneDetails = phone => {
     spinner('block');
@@ -66,10 +107,9 @@ const loadPhoneDetails = phone => {
 const displayPhoneDetails = phone => {
     console.log(phone);
     const container = document.getElementById('phone-details-container');
-    container.textContent = '';
     spinner('none');
     error('');
-    document.getElementById('phones-container').textContent = '';
+    textContent();
     const div = document.createElement('div');
     div.innerHTML = `
     <div class="card w-50 mx-auto ">
@@ -78,16 +118,16 @@ const displayPhoneDetails = phone => {
             <h5 class="card-title"><span class="fw-bolder">Name:</span> ${phone.name}</h5>
             <h6 class="card-text"><span class="fw-bolder">Brand:</span> ${phone.brand}</h6>
             <p class="card-text"><span class="fw-bolder">Release Date:</span> ${phone.releaseDate ? phone.releaseDate : 'No release date found'}</p>
-            <p class="card-text"><span class="fw-bolder">Chipset:</span> ${phone.mainFeatures.chipSet ? phone.mainFeatures.chipSet : ''}</p>
-            <p class="card-text"><span class="fw-bolder">Display Size:</span> ${phone.mainFeatures.displaySize ? phone.mainFeatures.displaySize : ''}</p>
-            <p class="card-text"><span class="fw-bolder">Memory:</span> ${phone.mainFeatures.memory ? phone.mainFeatures.memory : ''}</p>
-            <p class="card-text"><span class="fw-bolder">Sensors:</span> ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors : ''}</p>
-            <p class="card-text"><span class="fw-bolder">Bluetooth:</span> ${phone.others.Bluetooth ? phone.others.Bluetooth : ''}</p>
-            <p class="card-text"><span class="fw-bolder">GPS:</span> ${phone.others.GPS ? phone.others.GPS : ''}</p>
-            <p class="card-text"><span class="fw-bolder">NFC:</span> ${phone.others.NFC ? phone.others.NFC : ''}</p>
-            <p class="card-text"><span class="fw-bolder">Radio:</span> ${phone.others.Radio ? phone.others.Radio : ''}</p>
-            <p class="card-text"><span class="fw-bolder">USB:</span> ${phone.others.USB ? phone.others.USB : ''}</p>
-            <p class="card-text"><span class="fw-bolder">WLAN:</span> ${phone.others.WLAN ? phone.others.WLAN : ''}</p>
+            <p class="card-text"><span class="fw-bolder">Chipset:</span> ${phone.mainFeatures.chipSet ? phone.mainFeatures.chipSet : 'No chipset found'}</p>
+            <p class="card-text"><span class="fw-bolder">Display Size:</span> ${phone.mainFeatures.displaySize ? phone.mainFeatures.displaySize : 'No display size found'}</p>
+            <p class="card-text"><span class="fw-bolder">Memory:</span> ${phone.mainFeatures.memory ? phone.mainFeatures.memory : 'No memeory found'}</p>
+            <p class="card-text"><span class="fw-bolder">Sensors:</span> ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors : 'NO sensor found'}</p>
+            <p class="card-text"><span class="fw-bolder">Bluetooth:</span> ${phone?.others?.Bluetooth ? phone.others.Bluetooth : 'No bluetooth found'}</p>
+            <p class="card-text"><span class="fw-bolder">GPS:</span> ${phone?.others?.GPS ? phone.others.GPS : 'No GPS found'}</p>
+            <p class="card-text"><span class="fw-bolder">NFC:</span> ${phone?.others?.NFC ? phone.others.NFC : 'No NFC found'}</p>
+            <p class="card-text"><span class="fw-bolder">Radio:</span> ${phone?.others?.Radio ? phone.others.Radio : 'No radio found'}</p>
+            <p class="card-text"><span class="fw-bolder">USB:</span> ${phone?.others?.USB ? phone.others.USB : 'No USB found'}</p>
+            <p class="card-text"><span class="fw-bolder">WLAN:</span> ${phone?.others?.WLAN ? phone.others.WLAN : 'No WLAN found'}</p>
             
         </div>   
     </div>
